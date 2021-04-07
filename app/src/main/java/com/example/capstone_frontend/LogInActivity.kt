08155c46ab1a40a2e4024704fe4802070ee8ec66
mib912 <jungb912@daum.net>
2021-04-08 00:29:17 +0900

    whoooooooooly

--- app/src/main/java/com/example/capstone_frontend/LogInActivity.kt
+++ app/src/main/java/com/example/capstone_frontend/LogInActivity.kt
@@ -21,7 +21,7 @@ class LogInActivity : AppCompatActivity() {
                 Toast.makeText(this, "토큰 정보 보기 실패", Toast.LENGTH_SHORT).show()
             } else if (tokenInfo != null) {
                 Toast.makeText(this, "토큰 정보 보기 성공", Toast.LENGTH_SHORT).show()
-                val intent = Intent(this, LogoutActivity::class.java)
+                val intent = Intent(this, ChooseTypeActivity::class.java) //->LogoutActivity::class.java
                 startActivity(intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP))
             }
         }
