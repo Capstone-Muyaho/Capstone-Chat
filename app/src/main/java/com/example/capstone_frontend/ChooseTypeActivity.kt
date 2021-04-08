71b3bb7b59410f7e16b42a8c7cfd9d199dd06bed
mib912 <jungb912@daum.net>
2021-04-09 01:44:21 +0900

    부모(P)/자식(C) 선택 & 별명 설정

--- app/src/main/java/com/example/capstone_frontend/ChooseTypeActivity.kt
+++ app/src/main/java/com/example/capstone_frontend/ChooseTypeActivity.kt
@@ -1,30 +1,53 @@
 package com.example.capstone_frontend
 
+import android.annotation.SuppressLint
 import android.content.Intent
+import android.content.SharedPreferences
 import android.os.Bundle
-import android.widget.Button
-import android.widget.ImageButton
-import android.widget.Toast
+import android.view.KeyEvent
+import android.view.View
+import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
+import com.kakao.sdk.common.KakaoSdk.type
 
 class ChooseTypeActivity : AppCompatActivity() {
+    @SuppressLint("WrongViewCast")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_type)
 
-        val button1 = findViewById<ImageButton>(R.id.btnGrandma)
-        val button2 = findViewById<ImageButton>(R.id.btnSon)
+        val button1 = findViewById<RadioButton>(R.id.btnGrandma)
+        val button2 = findViewById<RadioButton>(R.id.btnSon)
+        val button3 = findViewById<Button>(R.id.btnNick)
+        val edittext = findViewById<EditText>(R.id.edit_nickN)
+
         button1.setOnClickListener {
-            val intent = Intent(this@ChooseTypeActivity, LogoutActivity::class.java) //부모 메인화면으로 가게 해야함
-            Toast.makeText(applicationContext, "버튼 클릭 성공(부모)", Toast.LENGTH_SHORT).show()
-            //버튼 클릭시 Toast 메세지"버튼 클릭 성공" 출력
-            startActivity(intent)
+            val Stype = "P"
+            Toast.makeText(applicationContext, Stype, Toast.LENGTH_SHORT).show()
+            //버튼 클릭시 Toast 메세지 type 출력
+            button3.setOnClickListener {
+                val inputNick = edittext.text.toString()
+                Toast.makeText(this@ChooseTypeActivity, inputNick, Toast.LENGTH_SHORT).show()
+
+                val intent = Intent(this, LogoutActivity::class.java)
+                intent.putExtra("type", Stype)
+                intent.putExtra("nickname", inputNick)
+                startActivity(intent)
+            }
         }
         button2.setOnClickListener {
-            val intent = Intent(this@ChooseTypeActivity,LogoutActivity::class.java) //자식 메인화면으로 가게 해야함
-            Toast.makeText(applicationContext, "버튼 클릭 성공(자녀)", Toast.LENGTH_SHORT).show()
-            //버튼 클릭시 Toast 메세지"버튼 클릭 성공" 출력
-            startActivity(intent)
+            val Stype = "C"
+            Toast.makeText(applicationContext, Stype, Toast.LENGTH_SHORT).show()
+
+            button3.setOnClickListener {
+                val inputNick = edittext.text.toString()
+                Toast.makeText(this@ChooseTypeActivity, inputNick, Toast.LENGTH_SHORT).show()
+
+                val intent = Intent(this, LogoutActivity::class.java)
+                intent.putExtra("type", Stype)
+                intent.putExtra("nickname", inputNick)
+                startActivity(intent)
+            }
         }
     }
 }
--- app/src/main/java/com/example/capstone_frontend/LogoutActivity.kt
+++ app/src/main/java/com/example/capstone_frontend/LogoutActivity.kt
@@ -5,6 +5,7 @@ import android.content.Intent.FLAG_ACTIVITY_CLEAR_TOP
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
 import com.kakao.sdk.user.UserApiClient
 import kotlinx.android.synthetic.main.activity_logout.*
@@ -16,10 +17,18 @@ class LogoutActivity : AppCompatActivity() {
 
         UserApiClient.instance.me { user, error ->
             id.text = "회원번호: ${user?.id}"
-            nickname.text = "닉네임: ${user?.kakaoAccount?.profile?.nickname}"
             profileimage_url.text = "프로필 링크: ${user?.kakaoAccount?.profile?.profileImageUrl}"
             thumbnailimage_url.text = "썸네일 링크: ${user?.kakaoAccount?.profile?.thumbnailImageUrl}"
         }
+
+        val tv_type = findViewById<TextView>(R.id.selectedType)
+        val tv_nickname = findViewById<TextView>(R.id.nickname)
+
+        val Stype = intent.getStringExtra("type")
+        val nickname = intent.getStringExtra("nickname")
+
+        tv_type.text = "유형: ${Stype}"
+        tv_nickname.text = "닉네임: ${nickname}"
 
         val kakao_logout_button: Button = findViewById(R.id.kakao_logout_button)
         kakao_logout_button.setOnClickListener {
