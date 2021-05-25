a880d11eae0540ed971197ec716ec747c78b5ea5
ByungKookKoo <byunguki@kyonggi.ac.kr>
2021-05-25 20:46:33 +0900

    file

---
+++ app/src/main/java/com/muyaho/hyojason/ChatType.java
@@ -0,0 +1,9 @@
+package com.muyaho.hyojason;
+
+public class ChatType {
+    public static final int LEFT_MESSAGE = 0;
+    public static final int CENTER_MESSAGE = 1;
+    public static final int RIGHT_MESSAGE = 2;
+    public static final int LEFT_IMAGE = 3;
+    public static final int RIGHT_IMAGE = 4;
+}
---
+++ app/src/main/java/com/muyaho/hyojason/retrofit/ApiService.java
@@ -0,0 +1,13 @@
+package com.muyaho.hyojason.retrofit;
+
+import okhttp3.MultipartBody;
+import retrofit2.Call;
+import retrofit2.http.Multipart;
+import retrofit2.http.POST;
+import retrofit2.http.Part;
+
+public interface ApiService {
+    @Multipart
+    @POST("/upload")
+    Call<Result> uploadImage(@Part MultipartBody.Part image);
+}
